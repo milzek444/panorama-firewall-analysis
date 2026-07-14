4c0173ae6992e94cdf5a1ae39c55a205e0faa5ac
thejokeson-you <milansaleh2004@gmail.com>
2026-07-14 12:58:55 +0100

    Add initial file-level/module documentation

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+main.py
+
+This module contains the main code for the project, including parsing functions, drift
+detection, and reporting.
+
+TODO: Later on, determine whether the code requires splitting into separate modules (if it becomes too difficult to
+read)
+"""
