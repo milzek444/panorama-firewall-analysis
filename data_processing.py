d3b288a8657bd46add3b0731a4d74411c8fb4642
thejokeson-you <milansaleh2004@gmail.com>
2026-08-02 14:01:40 +0100

    Add docstrings

--- data_processing.py
+++ data_processing.py
@@ -7,3 +7,46 @@ on firewall XML configuration and CSV traffic data prior to main  analysis
 """
 
 
+def parse_xml():
+    """
+    Parses the Panorama XML running configuration and extracts relevant firewall policy and configuration data.
+    :param:
+    :return:
+    """
+    print("Calling parse_xml...")
+
+def parse_objects():
+    """
+    Extracts and processes Panorama objects from the running configuration.
+    :param:
+    :return:
+    """
+    print("Calling parse_objects...")
+
+def search_traffic():
+    """
+    Searches through firewall traffic log CSV data to identify recent activity associated with objects.
+    :param:
+    :return:
+    """
+    print("Calling search_traffic...")
+
+def reverse_dns():
+    """
+    Performs reverse DNS lookups to resolve IP addresses to their corresponding hostnames for validation.
+    :param:
+    :return:
+    """
+    print("Calling reverse_dns...")
+
+def normalise_policy():
+    """
+    Normalises given firewall data into a consistent tuple format suitable for automated comparison
+    and analysis.
+    :param:
+    :return:
+    """
+    print("Calling normalise_policy...")
+
+
+
