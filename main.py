b67f8887c229e8baf63c3e2d13f6874cd8ac76ff
thejokeson-you <milansaleh2004@gmail.com>
2026-08-05 13:09:38 +0100

    Add initialisers to main.py

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ detection, and reporting.
 TODO: Later on, determine whether the code requires splitting into separate modules (if it becomes too difficult to
 read)
 """
+def main():
+    print("Calling entry method...")
+
+
+def connect_to_panorama():
+    """
+    A separate method which will hold the code for connecting to Panorama
+    """
+
+if __name__ == "__main__":
+    main()
