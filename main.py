399ed5ecac6e24286516c24e953a7b0b4c26c952
thejokeson-you <milansaleh2004@gmail.com>
2026-08-05 14:05:58 +0100

    Add Panorama connection function

--- main.py
+++ main.py
@@ -13,8 +13,40 @@ def main():
 
 def connect_to_panorama():
     """
-    A separate method which will hold the code for connecting to Panorama
+    A separate method which will hold the code for connecting to Panorama, which may or may not be needed
     """
+    panorama_ip = input("Enter Panorama Address: ").strip()
+    api_k = input("Enter valid API key: ").strip()
+
+    days = 45   # traffic log should be 45 days
+
+    connection = PanoramaConnection(hostname=panorama_ip, api_key=api_k)
+
+    try:
+        connection.connect()
+        print("SUCCESS: Connection has been authenticated.")
+
+        running_config_xml = connection.download_running_config()
+        traffic_logs_csv = connection.download_traffic_logs(days=days)
+        connection.disconnect()
+
+        print("\nINITIALISING PROCESSING PHASE.....")
+        print("\nExtracting structural objects context...")
+        objects_registry = parse_objects(running_config_xml)
+
+        print("Gathering active rule blocks from policy engine...")
+        raw_rules = parse_xml_policies(running_config_xml)
+
+        print("Generating normalized firewall rules...")
+        final_rules = normalise_firewall_rules(raw_rules, objects_registry)
+
+        print(f"\n===================================================")
+        print(f"PROCESS COMPLETE: processed {len(final_rules)} firewall rule dimensions successfully.")
+        print(f"===================================================")
+
+    except (ConnectionError, RuntimeError) as err:
+        print(f"\n[CRITICAL ERROR] Run execution halted: {err}")
+
 
 if __name__ == "__main__":
     main()
