1a1f3ecc6bce76d7ab47ac00e44a8b7447be7213
thejokeson-you <milansaleh2004@gmail.com>
2026-08-05 15:03:57 +0100

    Remove unneeded comments

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-"""
-main.py
-
-This module contains the main code for the project, including parsing functions, drift
-detection, and reporting.
-
-TODO: Later on, determine whether the code requires splitting into separate modules (if it becomes too difficult to
-read)
-"""
 from panos.panorama import Panorama
 from panos.errors import PanDeviceError
 import xml.etree.ElementTree as ET
